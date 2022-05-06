9fe041e8a0811301e8a8e985de3749320f6e0636
Conslatekoyo <conslatekoyo@gmail.com>
2022-05-07 00:18:55 +0300

    original commit

---
+++ 01_Day_Introduction/Day 1/introduction.py
@@ -0,0 +1,87 @@
+#This is how we write a comment in python
+#We use it to explain a code in python.
+"""When we want to write a multiline comment in python this is what we use. Here are some of the arithmetic symbols used in python
++ -addition
+- substraction
+% modulus(used to check the remainder 3%2==1 and 2%3==2 this is because whenever we modulus of a small number by a big number, the modulus is always the small number because a small number cannot modulus a big number)
+// floor operators(used to divide whole numbers)
+** exponential symbol used to raise a number to a given power
+integers include zeros ones and positive numbers i.e 0,1,23,456
+float include decimal numbers
+complex includes numbers and letters"""
+
+#Exercise: Level 1
+#Check the python version you are using
+#python--version
+#Open the python interactive shell and do the following operations. The operands are 3 and 4.
+#addition(+)
+print(3+4)
+#subtraction(-)
+print(4-3)
+#multiplication(*)
+print(3*4)
+#modulus(%)
+print(3%4)
+#division(/)
+print(3/4)
+#exponential(**)
+print(3**4)
+#floor division operator(//)
+print(3//4)
+#Write strings on the python interactive shell. The strings are the following:
+#Your name
+print("Conslate")
+#Your family name
+print("Koyo")
+#Your country
+print("Kenya")
+#I am enjoying 30 days of python
+print("I am enjoying 30days of python")
+#Check the data types of the following data:
+#10
+x=10
+print(type(x))
+#9.8
+y=9.8
+print(type(y))
+#3.14
+z=3.14
+print(type(z))
+#4 - 4j
+a=3.14
+print(type(a))
+#['Asabeneh', 'Python', 'Finland']
+names=['Asabeneh', 'Python', 'Finland']
+print(type(names))
+#Your name
+name="Conslate"
+print(type(name))
+#Your family name
+family_name="Koyo"
+print(type(family_name))
+#Your country
+country="Kenya"
+print(type(country))
+
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+num=7
+print(num)
+fl=2.4
+print(fl)
+compl=1.3j
+print(compl)
+string="Python"
+print(string)
+boolean=True
+print(boolean)
+list=[volvo,subaru,porche,jeep]
+print(list)
+tuple=(2,5,6,7,6,8,9,4)
+print(tuple)
+set={3,4,5,6,7,8,9,0}
+print(set)
+dictionary={shoes:heels,bag:versace}
+#Find an Euclidian distance between (2, 3) and (10, 8)
+distance_one=(2,3)
+distance_two=(10,8)
+print(distance_two-distance_one)
