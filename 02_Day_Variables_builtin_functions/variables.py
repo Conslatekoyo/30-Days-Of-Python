fc752cfb2c93b9fcfa26eb5f71aa962921e09b54
Conslatekoyo <conslatekoyo@gmail.com>
2022-05-09 23:25:15 +0300

    original commit

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,91 @@
+# """When naming variables in python,we use  the (snake_case) naming convention.The underscore is used if te variable name is more than one word"""
 
-# Variables in Python
+# # Write a python comment saying 'Day 2: 30 Days of python programming' 
+#         # 'Day 2: 30 days of python programming'
+# #  Declare a first name variable and assign a value to it
+# from itertools import product
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
-    }
 
-# Printing the values stored in the variables
+# first_Name="Conslate"
+# print(first_Name)
+# # Declare a last name variable and assign a value to it
+# last_Name="koyo"
+# print(last_Name)
+# # Declare a full name variable and assign a value to it
+# full_Name="conslate koyo"
+# print(full_Name)
+# # Declare a country variable and assign a value to it
+# country="Kenya"
+# print(country)
+# # Declare a city variable and assign a value to it
+# city="Nairobi"
+# print(city)
+# # Declare an age variable and assign a value to it
+# age= 35
+# print(age)
+# # Declare a year variable and assign a value to it
+# year=2022
+# print(year)
+# # Declare a variable is_married and assign a value to it
+# is_married=True
+# print(is_married)
+# # Declare a variable is_true and assign a value to it
+# is_true="probably"
+# print(is_true)
+# # Declare a variable is_light_on and assign a value to it
+# is_light=50
+# # Declare multiple variable on one line
+# print(first_Name,last_Name,age,country,city)
+# # Check the data type of all your variables using type() built-in function
+# print(type(first_Name))
+# print(type(last_Name))
+# print(type(full_Name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(is_true))
+# print(type(is_light))
+# # Using the len() built-in function, find the length of your first name
+# print(len(first_Name))
+# # Compare the length of your first name and your last name
+# print(len(first_Name)!=len(last_Name))
+# # Declare 5 as num_one and 4 as num_two
+# num_one=5
+# print(num_one)
+# num_two=4
+# print(num_two)
+# # Add num_one and num_two and assign the value to a variable total
+# total=num_one+num_two
+# print(total)
+# # Subtract num_two from num_one and assign the value to a variable diff
+# diff=num_one-num_two
+# print(diff)
+# # Multiply num_two and num_one and assign the value to a variable product
+# product=num_two*num_one
+# print(product)
+# # Divide num_one by num_two and assign the value to a variable division
+# division=num_two/num_one
+# print(division)
+# # Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+# remainder=num_two%num_one
+# print(remainder)
+# # Calculate num_one to the power of num_two and assign the value to a variable exp
+# exp=num_one**num_two
+# print(exp)
+# # Find floor division of num_one by num_two and assign the value to a variable floor_division
+# floor_division=num_one//num_two
+# # The radius of a circle is 30 meters.
+# # Calculate the area of a circle and assign the value to a variable name of area_of_circle
+# radius=30
+# area_of_a_circle=3.142*(radius**2)
+# print(area_of_a_circle)
+# # Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+# circum_of_circle=3.142*(2*radius)
+# print(circum_of_circle)
+# Take radius as user input and calculate the area.
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
 
-# Declaring multiple variables in one line
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
-
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+# Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+fi
+# Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
